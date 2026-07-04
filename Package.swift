db3522a04e6455688791502f861f6a050f2537ff
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-07-05 04:12:22 +0530

    exclude rust bridge completely

--- Package.swift
+++ Package.swift
@@ -20,37 +20,37 @@ let package = Package(
         )
     ],
     targets: [
-        .binaryTarget(
-            name: "RustBridgeLib",
-            path: "RustBridge/lib/RustBridge.xcframework"
-        ),
+//        .binaryTarget(
+//            name: "RustBridgeLib",
+//            path: "RustBridge/lib/RustBridge.xcframework"
+//        ),
         .binaryTarget(
             name: "IDevice",
             url: "https://github.com/jkcoxson/idevice/releases/download/v0.1.64/idevice-xcframework-v0.1.64.zip",
             checksum: "b8250402a23c850f80b9be1d4add309aae6c935ee6a797b73616e4d8f170be5d"
         ),
-        .target(
-            name: "RustBridge",
-            dependencies: ["RustBridgeLib"],
-            path: "RustBridge",
-            exclude: [
-                "Cargo.toml",
-                "Cargo.lock",
-                "src",
-                "Makefile",
-                "lib",
-                // "MinimuxerBridgeIdevice.swift"
-            ],
-            sources: [
-                "MinimuxerBridge.swift",
-                // "MinimuxerBridgeIdevice.swift"
-            ]
-        ),
+//        .target(
+//            name: "RustBridge",
+//            dependencies: ["RustBridgeLib"],
+//            path: "RustBridge",
+//            exclude: [
+//                "Cargo.toml",
+//                "Cargo.lock",
+//                "src",
+//                "Makefile",
+//                "lib",
+//                // "MinimuxerBridgeIdevice.swift"
+//            ],
+//            sources: [
+//                "MinimuxerBridge.swift",
+//                // "MinimuxerBridgeIdevice.swift"
+//            ]
+//        ),
         // MARK: Main SPM target
         .target(
             name: "Minimuxer",
             dependencies: [
-                "RustBridge",
+//                "RustBridge",
                 "IDevice",
                 .product(name: "ZIPFoundation", package: "ZIPFoundation")
             ],

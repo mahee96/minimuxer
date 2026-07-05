ad4f473091ec70356ffe32b307bb869cc6557c43
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-07-05 14:39:09 +0530

    updated to use latest idevice crate from sidestore fork

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDevice",
-            url: "https://github.com/SideStore/idevice/releases/download/v0.1.64-ss-499065c-ss-5c8e027/idevice-xcframework-v0.1.64-ss-499065c-ss-5c8e027.zip",
-            checksum: "95d5e075bdc8d64f0d2df976e53d9f62129c76cba3b58be95ca11c3acdebf75f"
+            url: "https://github.com/SideStore/idevice/releases/download/v0.1.64-ss-b1dac43/idevice-xcframework-v0.1.64-ss-b1dac43.zip",
+            checksum: "2d0d102051ee0633d451e95faecf5536f85e35a2cb090bde48a85e49d6259e95"
         ),
         // MARK: Main SPM target
         .target(

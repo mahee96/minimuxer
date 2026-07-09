446e241742bc39c1b11245b42274f154e804acbd
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-07-09 06:05:12 +0530

    update logging to ignore prefix tagging if text is just newline chars

--- Sources/MinimuxerImpl.swift
+++ Sources/MinimuxerImpl.swift
@@ -369,7 +369,12 @@ private func getTag(level: String) -> String {
 
 @inline(__always)
 func debugLog(_ text: @autoclosure () -> String) {
-    print("\(getTag(level: "DEBUG"))\(text())")
+    let message = text()
+    if !message.isEmpty && message.allSatisfy({ $0 == "\n" || $0 == "\r" }) {
+        print(message)
+    } else {
+        print("\(getTag(level: "DEBUG"))\(message)")
+    }
 }
 
 

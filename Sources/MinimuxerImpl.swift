a51973a24c308ff13fea06608763910fba3d7468
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-07-09 06:23:03 +0530

    removed timezone info from logs

--- Sources/MinimuxerImpl.swift
+++ Sources/MinimuxerImpl.swift
@@ -360,8 +360,7 @@ final internal class MinimuxerImpl: MinimuxerAPI {
 
 private func getTag(level: String) -> String {
     let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
     formatter.locale = Locale(identifier: "en_US_POSIX")
     let timestamp = formatter.string(from: Date())
     return "\(timestamp) \(level): "

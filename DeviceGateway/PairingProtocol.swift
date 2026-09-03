a95f92b5c3bfc27b378aa92063e66027a246fbca
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-09-03 13:44:40 +0530

    prepare pairing protocol to be moved

--- DeviceGateway/PairingProtocol.swift
+++ DeviceGateway/PairingProtocol.swift
@@ -1,11 +1,33 @@
 //
 //  PairingProtocol.swift
-//  Minimuxer
+//  MinimuxerCommon
 //
 //  Created by Magesh K on 7/8/26.
 //  Copyright © 2026 SideStore. All rights reserved.
 //
+
 import Foundation
+
+public enum PairingError: LocalizedError, CustomStringConvertible, Sendable {
+    case unreadable(String)
+    case invalidPlist(String)
+    case incomplete(missingRP: [String], missingLockdown: [String])
+
+    public var errorDescription: String? {
+        switch self {
+        case .unreadable(let reason):
+            return "The pairing file could not be read: \(reason)"
+        case .invalidPlist(let reason):
+            return "The pairing file could not be parsed as a property list (plist): \(reason)"
+        case .incomplete(let missingRP, let missingLockdown):
+            return "The pairing file is incomplete. Missing Remote Pairing attributes: \(missingRP.joined(separator: ", ")); missing Lockdown attributes: \(missingLockdown.joined(separator: ", "))."
+        }
+    }
+
+    public var description: String {
+        errorDescription ?? "Pairing error"
+    }
+}
 
 public enum PairingProtocol: String, Codable, CustomStringConvertible, Sendable {
     case rppairing = "rppairing"
@@ -16,21 +38,31 @@ public enum PairingProtocol: String, Codable, CustomStringConvertible, Sendable 
         return self.rawValue
     }
 
-
     public static func validatePairingFile(from plist: [String: Any]?) throws -> PairingProtocol {
         guard let plist = plist else {
-            throw DeviceGatewayError(.invalidPairingFile, reason: "The file could not be parsed as a property list (plist).")
+            throw PairingError.invalidPlist("The file could not be parsed as a property list (plist).")
         }
 
-        let requiredRPKeys = ["private_key", "public_key", "identifier"]
+        let requiredRPKeys = [
+            "private_key",
+            "public_key",
+            "identifier"
+        ]
         let missingRPKeys = requiredRPKeys.filter { plist[$0] == nil }
         if missingRPKeys.isEmpty {
             return .rppairing
         }
 
         let requiredLockdownKeys = [
-            "WiFiMACAddress", "SystemBUID", "RootPrivateKey", "HostPrivateKey",
-            "HostID", "RootCertificate", "UDID", "EscrowBag", "HostCertificate",
+            "WiFiMACAddress",
+            "SystemBUID",
+            "RootPrivateKey",
+            "HostPrivateKey",
+            "HostID",
+            "RootCertificate",
+            "UDID",
+            "EscrowBag",
+            "HostCertificate",
             "DeviceCertificate"
         ]
         let missingLockdownKeys = requiredLockdownKeys.filter { plist[$0] == nil }
@@ -38,9 +70,9 @@ public enum PairingProtocol: String, Codable, CustomStringConvertible, Sendable 
             return .lockdown
         }
 
-        throw DeviceGatewayError(
-            .invalidPairingFile,
-            reason: "The pairing file is incomplete. Missing Remote Pairing attributes: \(missingRPKeys.joined(separator: ", ")); missing Lockdown attributes: \(missingLockdownKeys.joined(separator: ", "))."
+        throw PairingError.incomplete(
+            missingRP: missingRPKeys,
+            missingLockdown: missingLockdownKeys
         )
     }
 }
